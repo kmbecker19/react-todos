c4d824a8e1ce454398445e050a074e6057c69c44
Kyle Becker <kmb08625@uga.edu>
2025-03-11 16:24:15 -0400

    Build Header in App

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Heading, Flex, Separator } from '@chakra-ui/react';
+
+export default function Header() {
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1rem"
+      bg="gray.400"
+      width="100%"
+      position="fixed"
+      top="0"
+      left="0"
+      right="0"
+      zIndex="1000"
+    >
+      <Flex align="center" as="nav" mr={5}>
+        <Heading as="h1" size="sm">Todos</Heading>
+        <Separator />
+      </Flex>
+    </Flex>
+  );
+}
