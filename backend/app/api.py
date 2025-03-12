59936bb01af11dd775e3e4adb42d406fb558fe05
Kyle Becker <kmb08625@uga.edu>
2025-03-12 12:31:48 -0400

    Add sqlmodel setup to api app

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from sqlmodel import create_engine, Session, SQLModel, select
+from pathlib import Path
+from typing import Annotated
 
 
-app = FastAPI()
+# Set up SQL Engine
+db_path = Path().absolute() / 'todos.db'
+sqlite_url = f'sqlite:///{db_path}'
+connect_args = {'check_same_thread': False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+# Session Dependency
+SessionDep = Annotated[Session, Depends(get_session)]
 
 origins = [
     "http://localhost:5173",
