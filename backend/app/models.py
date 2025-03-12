09f85913b2fd9993dca27f9c45d67cd3dd25c3e8
Kyle Becker <kmb08625@uga.edu>
2025-03-12 16:52:55 -0400

    Add due date to models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,8 +1,15 @@
 from sqlmodel import SQLModel, Field
 from uuid import UUID, uuid4
+from typing import Annotated
+from fastapi import Query
+
+# Force Dates to fit YYYY-MM-DD format
+date_pattern = r'^[1-2]\d{3}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$'
+DateStr = Annotated[str, Query(pattern=date_pattern)]
 
 class TodoBase(SQLModel):
     item: str | None = Field(index=True, default=None, unique=True)
+    due_date: DateStr | None = Field(index=True, default=None)
 
 class Todo(TodoBase, table=True):
     id: str = Field(primary_key=True, default_factory=lambda: str(uuid4()))
@@ -16,3 +23,4 @@ class TodoCreate(TodoBase):
 
 class TodoUpdate(TodoBase):
     item: str | None = None
+    due_date: DateStr | None = None
