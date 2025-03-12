81edf0fb1ff43d25bf9d525ca0e3b3499cf8dd4f
Kyle Becker <kmb08625@uga.edu>
2025-03-12 12:39:10 -0400

    Add GET and POST methods for todo

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from sqlmodel import create_engine, Session, SQLModel, select
 from pathlib import Path
 from typing import Annotated
+
+from .models import Todo, TodoPublic, TodoCreate, TodoUpdate
 
 
 # Set up SQL Engine
@@ -12,6 +14,7 @@ sqlite_url = f'sqlite:///{db_path}'
 connect_args = {'check_same_thread': False}
 engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     SQLModel.metadata.create_all(engine)
@@ -19,9 +22,11 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+
 def get_session():
     with Session(engine) as session:
         yield session
+
 
 # Session Dependency
 SessionDep = Annotated[Session, Depends(get_session)]
@@ -51,6 +56,25 @@ async def read_root() -> dict:
     return {'"message": "Welcome to your todo list."'}
 
 
+# SQL Server Routes
+@app.get('/sql/todo', response_model=list[TodoPublic])
+def read_sql_todos(session: SessionDep):
+    todos = session.exec(select(Todo)).all()
+    if not todos:
+        raise HTTPException(status_code=404, message='Todos not found')
+    return todos
+
+
+@app.post('/sql/todo', response_model=TodoPublic)
+def create_sql_todo(session: SessionDep, todo_create: TodoCreate):
+    todo_db = Todo.model_validate(todo_create)
+    session.add(todo_db)
+    session.commit()
+    session.refresh(todo_db)
+    return todo_db
+
+
+# Local Dict routes
 @app.get('/todo', tags=['todos'])
 async def read_todos() -> dict:
     return {"data": todos}
