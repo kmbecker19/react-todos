85623b759ea9c33b399097237764073d238fc234
Kyle Becker <kmb08625@uga.edu>
2025-03-17 16:07:08 -0400

    Add relationship between users and tasks in backend

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,23 +1,53 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 from uuid import UUID, uuid4
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import Query
+
+
+# User Models
+class UserBase(SQLModel):
+    username: str = Field(index=True)
+
+
+class User(UserBase, table=True):
+    id: str = Field(primary_key=True, default_factory=lambda: str(uuid4()))
+    password: str
+
+    todos: List['Todo'] = Relationship(back_populates='user')
+
+
+class UserPublic(UserBase):
+    id: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(UserBase):
+    username: str | None = None
+    password: str | None = None
 
 
 # Force Dates to fit YYYY-MM-DD format
 date_pattern = r'^[1-2]\d{3}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$'
 DateStr = Annotated[str, Query(pattern=date_pattern)]
 
+
 # Todo Models
 class TodoBase(SQLModel):
     item: str | None = Field(index=True, default=None, unique=True)
     due_date: DateStr | None = Field(index=True, default=None)
-    priority: Annotated[int | None, Query(ge=1, le=3)] = Field(index=True, default=None)
+    priority: Annotated[int | None, Query(ge=1, le=3)] = Field(
+        index=True, default=None)
     completed: bool = Field(index=True, default=False)
 
 
 class Todo(TodoBase, table=True):
     id: str = Field(primary_key=True, default_factory=lambda: str(uuid4()))
+
+    user_id: str | None = Field(default=None, foreign_key='user.id')
+    user: User | None = Relationship(back_populates='todos')
 
 
 class TodoPublic(TodoBase):
@@ -34,26 +64,3 @@ class TodoUpdate(TodoBase):
     due_date: DateStr | None = None
     priority: Annotated[int | None, Query(ge=1, le=3)] = None
     completed: bool | None = None
-
-
-# User Models
-class UserBase(SQLModel):
-    username: str = Field(index=True)
-
-
-class User(UserBase, table=True):
-    id: str = Field(primary_key=True, default_factory=lambda: str(uuid4()))
-    password: str
-
-
-class UserPublic(UserBase):
-    id: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class UserUpdate(UserBase):
-    username: str | None = None
-    password: str | None = None
